1a76eca977a958cf3b0b96bcb819305659de67c6
= <=>
2020-12-29 13:37:33 +0700

    merge.py is now a def

--- code/merge.py
+++ code/merge.py
@@ -1,21 +1,43 @@
 from PyPDF2 import PdfFileMerger
 from os import listdir
 
-input_dir = "C:\Git-hub\python-split-pdf\Base-file\\"
 
-merge_list = []
+#put this here to test
+print("input file to merge path here: ")
+mergePath = str(input())
+print("input merged file path here: ")
+mergedPath = str(input())
 
-for x in listdir(input_dir):
-    if not x.endswith('.pdf'):
-        continue
-    merge_list.append(input_dir + x)
 
-merger = PdfFileMerger()
-merge_list.sort(reverse=False)
+def merging_file(mergePath, mergedPath):    
+    #convert path to the right absoulute path
+    if mergePath.endswith('/'):
+        input_dir = mergePath + '/'
+    else:
+        input_dir = mergePath + '//'
 
-for pdf in merge_list:
-    merger.append(pdf)
-    print(pdf)
+    #convert path to the right absoulute path
+    if mergedPath.endswith('/'):
+        input_dir = mergedPath + '/'
+    else:
+        input_dir = mergedPath + '//'
 
-merger.write("C:\Git-hub\python-split-pdf\Merged\merged_pdf.pdf")
-merger.close()
+    merge_list = []
+
+    for x in listdir(input_dir):
+        if not x.endswith('.pdf'):
+            continue
+        merge_list.append(input_dir + x)
+
+    merger = PdfFileMerger()
+    merge_list.sort(reverse=False)
+
+    for pdf in merge_list:
+        merger.append(pdf)
+        print(pdf)
+
+    merged_file_path = "{}/merged_pdf.pdf".format(mergedPath)
+
+    merger.write(merged_file_path)
+    merger.close()
+    print("merge completed")
