585d8d72f031fba619f2324a1cd0db5cd348aa0d
pzohaycuoi <pzohaycuoi@gmail.com>
2021-01-02 13:50:44 +0700

    adding input split range

--- gui/ example.py
+++ gui/ example.py
@@ -7,8 +7,9 @@ layout = [
      sg.InputText(size=(20, 1), key='input_source_file'), sg.FileBrowse()],
     [sg.Text('Destination folder', size=(15, 1)),
      sg.InputText(size=(20, 1), key='input_destination_folder'), sg.FolderBrowse()],
-    [sg.Text('File name'), sg.InputText(size=(10, 1), key='input_file_name', enable_events=True),
-     sg.Text('Start number'), sg.InputText(size=(10, 1), key='input_file_number', enable_events=True)],
+    [sg.Text('File name'), sg.InputText(size=(8, 1), key='input_file_name', enable_events=True),
+     sg.Text('Start number'), sg.InputText(size=(4, 1), key='input_file_number', enable_events=True),
+     sg.Text('Split range'), sg.InputText(size=(4, 1), key=('input_step'), enable_events=True)],
     [sg.Radio('Ascending', 'rad1', pad=((0, 20), (0, 0)), key='sorting_asc'),
      sg.Radio('Descending', 'rad1', key='sorting_desc')],
     [sg.Button('OK'), sg.Button('Cancel')]
@@ -23,6 +24,7 @@ while True:
     destination_folder = values['input_destination_folder']
     file_name = values['input_file_name']
     file_number = values['input_file_number']
+    file_step = values['input_step']
 
     if event in (sg.WINDOW_CLOSED, 'Cancel'):
         break
@@ -30,10 +32,12 @@ while True:
         window['input_file_name'].update(file_name[:-1])
     if event == 'input_file_number' and file_number and file_number[-1] not in ('0123456789'):
         window['input_file_number'].update(file_number[:-1])
+    if event == 'input_file_number' and file_step and file_step[-1] not in ('0123456789'):
+        window['input_file_number'].update(file_step[:-1])
     if event in ('OK'):
         if not str(destination_folder).endswith('/'):
             destination_folder = str(destination_folder) + '/'
-            
+
         print(source_file)
         print(destination_folder)
 
