302616de5245d4e41f51e997fe549da4a4c67ff4
pzohaycuoi <pzohaycuoi@gmail.com>
2021-01-02 09:24:44 +0700

    example.py update

--- gui/ example.py
+++ gui/ example.py
@@ -4,22 +4,22 @@ sg.theme('DarkAmber')
 
 layout = [
     [sg.Text('Source file', size=(15, 1)),
-     sg.InputText(size=(20, 1)), sg.FileBrowse()],
+     sg.InputText(size=(20, 1), key='source_file'), sg.FileBrowse()],
     [sg.Text('Destination folder', size=(15, 1)),
-     sg.InputText(size=(20, 1)), sg.FileBrowse()],
-    [sg.Text('File name'), sg.InputText(size=(10, 1)), sg.Text(
-        'Start number'), sg.InputText(size=(10, 1))],
-    [sg.Radio('Ascending', 'rad1', pad=((0, 20), (0, 0))),
-     sg.Radio('Descending', 'rad1')],
+     sg.InputText(size=(20, 1), key='destination_folder'), sg.FileBrowse()],
+    [sg.Text('File name'), sg.InputText(size=(10, 1), key='file_name'), sg.Text(
+        'Start number'), sg.InputText(size=(10, 1), key='file_start_number')],
+    [sg.Radio('Ascending', 'rad1', pad=((0, 20), (0, 0)), key='sorting_asc'),
+     sg.Radio('Descending', 'rad1', key='sorting_desc')],
     [sg.Button('OK'), sg.Button('Cancel')]
 ]
 
 window = sg.Window('Title', layout)
 
-#event, values = window.read()
 while True:
     event, values = window.read()
     if event in (sg.WINDOW_CLOSED, 'Cancel'):
         break
+    # if event in 
 
 window.close()
