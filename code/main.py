ca17fced420b1e916e80344b65b1f495a85a266d
pzohaycuoi <pzohaycuoi@gmail.com>
2021-01-26 09:03:15 +0700

    Update main.py code readability and testing new GUI layout

--- code/main.py
+++ code/main.py
@@ -1,9 +1,11 @@
 from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
 import os
 
-class pdf_function:
 
-    def split_at_every(source_file, output_file_path, file_number_int, file_step_int, sorting):
+class pdf_function:
+    def split_at_every(
+        source_file, output_file_path, file_number_int, file_step_int, sorting
+    ):
 
         input_pdf = PdfFileReader(open(source_file, "rb"))
         pdf_len = input_pdf.numPages
@@ -11,20 +13,21 @@ class pdf_function:
 
         for ind, val in enumerate(page_numbers):
 
-            if(ind+1 != len(page_numbers)):
+            if ind + 1 != len(page_numbers):
                 output_1 = PdfFileWriter()
 
-                for page in range(page_numbers[ind], page_numbers[ind+1]):
+                for page in range(page_numbers[ind], page_numbers[ind + 1]):
 
-                    if sorting[0] == True:
-                        file_number_int += (1 / file_step_int)
-                    if sorting[1] == True:
-                        file_number_int -= (1 / file_step_int)
+                    if sorting[0] is True:
+                        file_number_int += 1 / file_step_int
+                    if sorting[1] is True:
+                        file_number_int -= 1 / file_step_int
 
                     page_data = input_pdf.getPage(page)
                     output_1.addPage(page_data)
-                    output_1_filename = '{}_{}.pdf'.format(
-                        output_file_path, int(file_number_int))
+                    output_1_filename = "{}_{}.pdf".format(
+                        output_file_path, int(file_number_int)
+                    )
                     print(output_1_filename)
 
                 output_stream1 = open(output_1_filename, "wb")
@@ -37,46 +40,32 @@ class pdf_function:
                 for page in range(page_numbers[ind], pdf_len):
                     page_data = input_pdf.getPage(page)
                     output_final.addPage(page_data)
-                    output_final_filename = '{}_lastpage_{}.pdf'.format(output_file_path, page + 1)
+                    output_final_filename = "{}_lastpage_{}.pdf".format(
+                        output_file_path, page + 1
+                    )
                     print(output_final_filename)
 
                 outputStream2 = open(output_final_filename, "wb")
                 output_final.write(outputStream2)
                 outputStream2.close()
 
-
-    def checking_missing_file(file_path, smallest_file_number, largest_file_number):
-        dirPath = listdir(file_path)
-
-        correct_file_list = []
-
-        for i in range(largest_file_number - smallest_file_number):
-            correct_file_list.append("{}.pdf".format(i))
-            i += 1
-
-        check = tuple(set(correct_file_list) - set(dirPath))
-        sort_check = sorted(check)
-
-        print(sort_check)
-
-
     def merging_file(merge_path, merged_path):
         # convert path to the right absoulute path
-        if merge_path.endswith('/'):
-            input_dir = merge_path + '/'
+        if merge_path.endswith("/"):
+            input_dir = merge_path + "/"
         else:
-            input_dir = merge_path + '//'
+            input_dir = merge_path + "//"
 
         # convert path to the right absoulute path
-        if merged_path.endswith('/'):
-            input_dir = merged_path + '/'
+        if merged_path.endswith("/"):
+            input_dir = merged_path + "/"
         else:
-            input_dir = merged_path + '//'
+            input_dir = merged_path + "//"
 
         merge_list = []
 
-        for x in listdir(input_dir):
-            if not x.endswith('.pdf'):
+        for x in os.listdir(input_dir):
+            if not x.endswith(".pdf"):
                 continue
             merge_list.append(input_dir + x)
 
@@ -92,3 +81,17 @@ class pdf_function:
         merger.write(merged_file_path)
         merger.close()
         print("merge completed")
+
+    def checking_missing_file(file_path, smallest_file_number, largest_file_number):
+        dirPath = os.listdir(file_path)
+
+        correct_file_list = []
+
+        for i in range(largest_file_number - smallest_file_number):
+            correct_file_list.append("{}.pdf".format(i))
+            i += 1
+
+        check = tuple(set(correct_file_list) - set(dirPath))
+        sort_check = sorted(check)
+
+        print(sort_check)
--- code/splitandmergeGUI.py
+++ code/splitandmergeGUI.py
@@ -3,68 +3,93 @@ import sys
 
 from main import pdf_function as splitFunc
 
-
-sg.theme('DarkAmber')
+sg.theme("DarkAmber")
 
 split_Func_Tab = [
-    [sg.Text('Source file', size=(15, 1)),
-     sg.InputText(size=(20, 1), key='input_source_file'), sg.FileBrowse()],
-    [sg.Text('Destination folder', size=(15, 1)),
-     sg.InputText(size=(20, 1), key='input_destination_folder'), sg.FolderBrowse()],
-    [sg.Text('File name'), sg.InputText(size=(8, 1), key='input_file_name', enable_events=True),
-     sg.Text('Start number'), sg.InputText(size=(4, 1), key='input_file_number', enable_events=True),
-     sg.Text('Split range'), sg.InputText(size=(4, 1), key=('input_step'), enable_events=True)],
-    [sg.Radio('Ascending', 'rad1', pad=((0, 20), (0, 0)), key='input_sorting_asc'),
-     sg.Radio('Descending', 'rad1', key='input_sorting_desc')]
+    [
+        sg.Text("Source file", size=(15, 1)),
+        sg.InputText(size=(20, 1), key="input_source_file"),
+        sg.FileBrowse(),
+    ],
+    [
+        sg.Text("Destination folder", size=(15, 1)),
+        sg.InputText(size=(20, 1), key="input_destination_folder"),
+        sg.FolderBrowse(),
+    ],
+    [
+        sg.Text("File name"),
+        sg.InputText(size=(8, 1), key="input_file_name", enable_events=True),
+        sg.Text("Start number"),
+        sg.InputText(size=(4, 1), key="input_file_number", enable_events=True),
+        sg.Text("Split range"),
+        sg.InputText(size=(4, 1), key=("input_step"), enable_events=True),
+    ],
+    [
+        sg.Radio("Ascending", "rad1", pad=((0, 20), (0, 0)), key="input_sorting_asc"),
+        sg.Radio("Descending", "rad1", key="input_sorting_desc"),
+    ],
 ]
 
-Merge_Func_Tab = [
+Merge_Func_Tab = [[sg.Text("Test")]]
 
+Check_Missing_Func_Tab = [[sg.Text("Test")]]
+
+layout = [
+    [
+        sg.TabGroup(
+            [[sg.Tab("Split PDF", split_Func_Tab), sg.Tab("Merge PDF", Merge_Func_Tab), sg.Tab("Check Missing File", Check_Missing_Func_Tab)]]
+        )
+    ],
+    [sg.Button("OK"), sg.Button("Cancel")]
 ]
 
-Check_Missing_Func_Tab = [
-    
-]
-
-layout = sg.TabGroup(
-    
-)
-
-window = sg.Window('Title', layout)
+window = sg.Window("Nam Beo", layout)
 
 while True:
     event, values = window.read()
 
-    if event in (sg.WINDOW_CLOSED, 'Cancel'):
+    if event in (sg.WINDOW_CLOSED, "Cancel"):
         break
 
-    source_file = values['input_source_file']
-    destination_folder = values['input_destination_folder']
-    file_name = values['input_file_name']
-    file_number = values['input_file_number']
-    file_step = values['input_step']
-    sorting_asc = values['input_sorting_asc']
-    sorting_desc = values['input_sorting_desc']
+    # source_file = values["input_source_file"]
+    # destination_folder = values["input_destination_folder"]
+    # file_name = values["input_file_name"]
+    # file_number = values["input_file_number"]
+    # file_step = values["input_step"]
+    # sorting_asc = values["input_sorting_asc"]
+    # sorting_desc = values["input_sorting_desc"]
 
-    if event == 'input_file_name' and file_name and file_name[-1] in ("'*<>?\|/:"".,`"):
-        window['input_file_name'].update(file_name[:-1])
-    if event == 'input_file_number' and file_number and file_number[-1] not in ('0123456789'):
-        window['input_file_number'].update(file_number[:-1])
-    if event == 'input_file_number' and file_step and file_step[-1] not in ('0123456789'):
-        window['input_file_number'].update(file_step[:-1])
-    if event in ('OK'):
-        if not str(destination_folder).endswith('/'):
-            destination_folder = str(destination_folder) + '/'
+    # if (
+    #     event == "input_file_name"
+    #     and file_name
+    #     and file_name[-1] in ("'*<>?\|/:" ".,`")
+    # ):
+    #     window["input_file_name"].update(file_name[:-1])
+    # if (
+    #     event == "input_file_number"
+    #     and file_number
+    #     and file_number[-1] not in ("0123456789")
+    # ):
+    #     window["input_file_number"].update(file_number[:-1])
+    # if (
+    #     event == "input_file_number"
+    #     and file_step
+    #     and file_step[-1] not in ("0123456789")
+    # ):
+    #     window["input_file_number"].update(file_step[:-1])
+    # if event in ("OK"):
+    #     if not str(destination_folder).endswith("/"):
+    #         destination_folder = str(destination_folder) + "/"
 
-        file_number_int = int(file_number)
-        file_step_int = int(file_step)
+    #     file_number_int = int(file_number)
+    #     file_step_int = int(file_step)
 
-        output_file_path = '{}{}'.format(
-            destination_folder, file_name)
+    #     output_file_path = "{}{}".format(destination_folder, file_name)
 
-        sorting = [sorting_asc, sorting_desc]
+    #     sorting = [sorting_asc, sorting_desc]
 
-        splitFunc.split_at_every(
-            source_file, output_file_path, file_number_int, file_step_int, sorting)
+    #     splitFunc.split_at_every(
+    #         source_file, output_file_path, file_number_int, file_step_int, sorting
+    #     )
 
 window.close()
