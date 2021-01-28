a1b13c0cd45f4e0b156df5560c931273ed25fbeb
pzohaycuoi <pzohaycuoi@gmail.com>
2021-01-28 12:10:21 +0700

    update to test gui

--- code/splitandmergeGUI.py
+++ code/splitandmergeGUI.py
@@ -70,17 +70,17 @@ layout = [[
     ]])
 ]]
 
-window = sg.Window("Nam Beo", layout)
-window2 = False
-window3 = False
+split_window = sg.Window("Nam Beo", layout)
+merge_select_file_window = False
+merge_sorting_file_window = False
 
 # GUI Function
 while True:
-    event, value = window.read()
+    event, value = split_window.read()
     if event == sg.WINDOW_CLOSED:
         break
 
-    # Window GUI Function
+    # split_window GUI Function
     source_file = value["split_inpt_source_file"]
     destination_folder = value["split_inpt_destination_folder"]
     file_name = value["split_inpt_file_name"]
@@ -91,13 +91,13 @@ while True:
     if (event == "split_inpt_file_name" and file_name
             and file_name[-1] in ("'*<>?\|/:"
                                   ".,`")):
-        window["split_inpt_file_name"].update(file_name[:-1])
+        split_window["split_inpt_file_name"].update(file_name[:-1])
     if (event == "input_file_number" and file_number
             and file_number[-1] not in ("0123456789")):
-        window["split_inpt_file_number"].update(file_number[:-1])
+        split_window["split_inpt_file_number"].update(file_number[:-1])
     if (event == "split_inpt_file_number" and file_step
             and file_step[-1] not in ("0123456789")):
-        window["split_inpt_file_number"].update(file_step[:-1])
+        split_window["split_inpt_file_number"].update(file_step[:-1])
     if event in ("OK"):
         if not str(destination_folder).endswith("/"):
             destination_folder = str(destination_folder) + "/"
@@ -109,9 +109,9 @@ while True:
         splitFunc.split_at_every(source_file, output_file_path,
                                  file_number_int, file_step_int, sorting)
 
-    # Window2 GUI Function
-    if event == "Nerge_inpt_select_file" and not window2:
-        window2 is True
+    # merge_select_file_window GUI Function
+    if event == "Nerge_inpt_select_file" and not merge_select_file_window:
+        merge_select_file_window is True
         Tree_Data = sg.TreeData()
 
         def display_file_in_folder(parent, source_folder):
@@ -132,7 +132,7 @@ while True:
         Merge_Source_Folder = value["Merge_inpt_source_folder"]
         display_file_in_folder('', Merge_Source_Folder)
 
-        Merge_Select_File_Window = [
+        Merge_Select_File_split_window = [
             [sg.Text("Select File")],
             [
                 sg.Tree(
@@ -149,26 +149,28 @@ while True:
                 )
             ],
             [
-                sg.Button("OK", key="Merge_Select_Window_OK"),
-                sg.Button("Cancel", key="Merge_Select_Window_Cancel")
+                sg.Button("OK", key="Merge_Select_split_window_OK"),
+                sg.Button("Cancel", key="Merge_Select_split_window_Cancel")
             ]
         ]
 
-        window2 = sg.Window("Select File", Merge_Select_File_Window)
+        merge_select_file_window = sg.Window(
+            "Select File", Merge_Select_File_split_window)
+        split_window.Hide()
 
     while True:
-        event2, value2 = window2.read()
-        if event2 in (sg.WINDOW_CLOSED, "Merge_Window_Cancel"):
-            window2.Close()
-            window2 = False
+        event2, value2 = merge_select_file_window.read()
+        if event2 in (sg.WINDOW_CLOSED,
+                      "Merge_Select_split_window_Cancel"):
+            merge_select_file_window.Close()
+            merge_select_file_window = False
+            split_window.UnHide()
             break
 
-    Merge_Select_File = value2["Merge_Input_Select_File"]
-
-    # Window3 GUI Function
+    # merge_sorting_file_window GUI Function
     if event == "input_sorting_file":
-        window3 = True
+        merge_sorting_file_window = True
 
-        Merge_Sorting_Window = []
+        merge_sorting_split_window = []
 
-window.close()
+split_window.close()
