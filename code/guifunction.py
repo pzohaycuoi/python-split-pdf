86d9b4d3c1642a2fdf865c8cb16e9c2d3d8219a2
pzohaycuoi <pzohaycuoi@gmail.com>
2021-01-28 21:35:40 +0700

    refactor code

--- code/guifunction.py
+++ code/guifunction.py
@@ -5,13 +5,10 @@ import PySimpleGUI as sg
 class GuiFunction():
     def add_files_in_folder(source_folder):
         files = os.listdir(source_folder)
-        file_path = []
         file_name = []
 
         for i in files:
-            file_path = os.path.join(source_folder, i)
             if i.endswith(".pdf"):
-                file_path.append(file_path)
                 file_name.append(i)
 
     def check_box_generator(file_name):
